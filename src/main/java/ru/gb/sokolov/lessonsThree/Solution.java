00936e1f8770f0a88869633afec4caf0341d4445
Artem-Ka220 <marktwe@mail.ru>
2022-04-20 22:08:11 +0300

    add homework three(1)

--- src/main/java/ru/gb/sokolov/lessonsThree/Solution.java
+++ src/main/java/ru/gb/sokolov/lessonsThree/Solution.java
@@ -1,4 +1,19 @@
 package ru.gb.sokolov.lessonsThree;
 
+import java.util.Arrays;
+
 public class Solution {
+    public static void main(String[] args) {
+        int [] arrays = new int[]{1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1};
+        System.out.println(Arrays.toString(arrays));
+
+        for (int i = 0; i < arrays.length; i++) {
+            if (arrays[i] == 0){
+                arrays[i] = 1;
+            }else {
+                arrays[i] = 0;
+            }
+        }
+        System.out.println(Arrays.toString(arrays));
+    }
 }
